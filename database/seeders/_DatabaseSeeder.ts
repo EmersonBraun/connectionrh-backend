0f01c2ec099f400fc043bd431684fd5d5af3afa5
EmersonBraun <emersonfbraun@gmail.com>
2020-11-25 10:25:28 -0300

    feat: create production seeders

--- database/seeders/_DatabaseSeeder.ts
+++ database/seeders/_DatabaseSeeder.ts
@@ -1,4 +1,12 @@
+import Env from '@ioc:Adonis/Core/Env'
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import ApplicationStatus from 'App/Models/ApplicationStatus'
+import Area from 'App/Models/Area'
+import ContractType from 'App/Models/ContractType'
+import Level from 'App/Models/Level'
+import PostCategory from 'App/Models/PostCategory'
+import RequestStatus from 'App/Models/RequestStatus'
+import Role from 'App/Models/Role'
 import User from 'App/Models/User'
 import {
   AddressFactory,
@@ -31,13 +39,22 @@ import {
   VacancyFactory,
 } from 'Database/factories'
 
-export default class AddressSeeder extends BaseSeeder {
+export default class DatabaseSeeder extends BaseSeeder {
   public async run () {
+    const state = Env.get('NODE_ENV') as String
+    console.log({state})
+    if (state && state !== 'development') {
+      await this.prodution()
+    } else {
+      await this.inTests()
+    }
+  }
+
+  public async inTests () {
     await AddressFactory.createMany(10)
     await AssetFactory.createMany(10)
     await CompanyFactory.createMany(10)
     await PhoneFactory.createMany(10)
-    await UserFactory.createMany(10)
     await ApplicationStatusFactory.createMany(10)
     await ApplicationFactory.createMany(10)
     await AreaFactory.createMany(10)
@@ -60,19 +77,60 @@ export default class AddressSeeder extends BaseSeeder {
     await RoleFactory.createMany(10)
     await SkillFactory.createMany(10)
     await StrongPointFactory.createMany(10)
+    await UserFactory.createMany(10)
     await VacancyFactory.createMany(10)
+  }
 
-    await User.createMany([
-      {
-        name: 'Teste 1',
-        email: 'test1@test.com',
-        password: 'secret',
-      },
-      {
-        name: 'Teste 2',
-        email: 'test2@test.com',
-        password: 'secret',
-      },
+  public async prodution () {
+    const roles =await Role.updateOrCreateMany('role',[
+      {role: 'Admin'},
+      {role: 'Candidato'},
+      {role: 'Empresa'},
     ])
+    roles.map(role => console.info(role.serialize()))
+    const applicationStatus = await ApplicationStatus.updateOrCreateMany('application_status',[
+      {application_status: 'Em análise'},
+      {application_status: 'Aprovado'},
+      {application_status: 'Reprovado'},
+    ])
+    applicationStatus.map(aplication => console.info(aplication.serialize()))
+    const areas = await Area.updateOrCreateMany('area',[
+      {area: 'TI'},
+      {area: 'ADM'},
+      {area: 'Contábeis'},
+      {area: 'Saúde'},
+    ])
+    areas.map(area => console.info(area.serialize()))
+    const contractTypes = await ContractType.updateOrCreateMany('contract_type',[
+      {contract_type: 'CLT'},
+      {contract_type: 'PJ'},
+    ])
+    contractTypes.map(contract => console.info(contract.serialize()))
+    const levels = await Level.updateOrCreateMany('level',[
+      {level: 'Técnico'},
+      {level: 'Bacharel'},
+      {level: 'Mestrado'},
+      {level: 'Doutorado'},
+    ])
+    levels.map(level => console.info(level.serialize()))
+    const postCategories = await PostCategory.updateOrCreateMany('post_category',[
+      {post_category: 'TI'},
+      {post_category: 'ADM'},
+      {post_category: 'Contábeis'},
+      {post_category: 'Saúde'},
+    ])
+    postCategories.map(categories => console.info(categories.serialize()))
+    const requestStatus = await RequestStatus.updateOrCreateMany('request_status',[
+      {request_status: 'Em análise'},
+      {request_status: 'Aprovado'},
+      {request_status: 'Recusado'},
+    ])
+    requestStatus.map(request => console.info(request.serialize()))
+    const users = await User.updateOrCreateMany('email',[
+      {name: 'Admin', password: 'ZX=P00!@123', email: 'conectionrh@gmail.com', email_confirmed: true, roleId: 1},
+      {name: 'Candidate', password: 'secret', email: 'candidate@gmail.com', email_confirmed: true, roleId: 2},
+      {name: 'Company', password: 'secret', email: 'company@gmail.com', email_confirmed: true, roleId: 3},
+    ])
+    users.map(user => console.info(user.serialize()))
   }
 }
