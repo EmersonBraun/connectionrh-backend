1e11ff6edbab6c3769990d54e5f3b8dffb999c2b
EmersonBraun <emersonfbraun@gmail.com>
2021-01-05 13:09:18 -0300

    fix: solve route unconsistenses and contactShcema

--- app/Validators/ContactSchema.ts
+++ app/Validators/ContactSchema.ts
@@ -1,6 +1,8 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { rules, schema } from '@ioc:Adonis/Core/Validator'
 
 export const ContactSchema = schema.create({
-  email: schema.string(),
+  email: schema.string({}, [
+    rules.email(),
+  ]),
   description: schema.string(),
 })
