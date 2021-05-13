606d0bcd19be9c7134dd00c37a8c981af25a7a9b
Pedro Cavalcante <pedro@hdom.com.br>
2021-05-12 21:57:36 -0300

    feat: add new files email and dependencies

--- app/Controllers/Http/MeetingsController.ts
+++ app/Controllers/Http/MeetingsController.ts
@@ -3,6 +3,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import MeetingsRepository from 'App/Repositories/MeetingsRepository'
 import { getErrors } from 'App/Services/MessageErros'
 import { MeetingSearchSchema } from 'App/Validators'
+import Interview from 'App/Models/Interview'
 import { MeetingSchema } from 'App/Validators/MeetingSchema'
 
 export default class MeetingsController {
@@ -49,7 +50,20 @@ export default class MeetingsController {
   async search ({ request, response }: HttpContextContract) {
     try {
       await request.validate({schema: MeetingSearchSchema})
+      const register = await this.repository.search(request.all())
+      const { data, statusCode, returnType, message, contentError } = register
+      /* eslint-disable */
+      const { user_id } = request.all()
+      const interviews = await Interview.query().where('user_id', user_id)
+      /* eslint-disable */
+      return response
+        .safeHeader('returnType', returnType)
+        .safeHeader('message', message)
+        .safeHeader('contentError', contentError)
+        .status(statusCode)
+        .json({ meetings: data, interviews: interviews })
     } catch (error) {
+      console.log(error)
       const msg = getErrors(error)
       // console.log(error.messages.errors)
       return response
@@ -59,15 +73,6 @@ export default class MeetingsController {
         .status(422)
         .json({})
     }
-
-    const register = await this.repository.search(request.all())
-    const { data, statusCode, returnType, message, contentError } = register
-    return response
-      .safeHeader('returnType', returnType)
-      .safeHeader('message', message)
-      .safeHeader('contentError', contentError)
-      .status(statusCode)
-      .json(data)
   }
 
   async show ({ params, response }: HttpContextContract) {
